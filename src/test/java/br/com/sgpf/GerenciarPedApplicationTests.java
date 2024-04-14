d3422f85dee46eadfab09eba030358d60899ab27
lucas <lucas@DESKTOP-BK41L68>
2024-04-14 20:06:25 -0300

    teste

---
+++ src/test/java/br/com/sgpf/GerenciarPedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarPedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
