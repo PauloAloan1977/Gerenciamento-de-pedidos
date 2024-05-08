6b308b4e008f2fb96fbcf824067d4514031ca7c9
Aluno <Aluno@ETEJK-LAB03-M07>
2024-05-07 21:07:23 -0300

    Crud

--- src/main/java/br/com/sgpf/entity/ClienteEntity.java
+++ src/main/java/br/com/sgpf/entity/ClienteEntity.java
@@ -1,5 +1,20 @@
 package br.com.sgpf.entity;
 
-public class ClienteEntity {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Cliente" , schema = "sgpf")
+public class ClienteEntity implements Serializable {
+
+	private static final long serialVersionUID = 1l;
+	@Id
+	@Column(name = "nome");
+	
+		
 
 }
--- src/main/java/br/com/sgpf/entity/ProdutoEntity.java
+++ src/main/java/br/com/sgpf/entity/ProdutoEntity.java
@@ -1,5 +1,31 @@
 package br.com.sgpf.entity;
 
-public class ProdutoEntity {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto" , schema = "sgpf")
+public class ProdutoEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_produto")
+	private Long idproduto;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "quantidade")
+	private String quantidade;
+	
+	
+	}
 
 }
